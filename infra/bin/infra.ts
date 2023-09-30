f13133342c128f8dc88e26a1ac5760b752a21c5c
muratariku0903 <18ba179r@rikkyo.ac.jp>
2023-09-30 22:16:25 +0900

    wip

--- infra/bin/infra.ts
+++ infra/bin/infra.ts
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 import 'source-map-support/register'
 import * as cdk from 'aws-cdk-lib'
-import { InfraStack } from '../lib/infra-stack'
+import { PipelineStack } from '../lib/pipeline-stack'
 
 const app = new cdk.App()
-new InfraStack(app, 'InfraStack', {})
+new PipelineStack(app, 'PipelineStack', {})
