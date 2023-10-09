21fa2ca5e5cf0d29053af614a72afb97f69513c4
muratariku0903 <18ba179r@rikkyo.ac.jp>
2023-10-09 19:55:26 +0900

    wip

--- infra/handlers/github-trigger-lambda.ts
+++ infra/handlers/github-trigger-lambda.ts
@@ -1,16 +1,43 @@
-import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
+import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
+import { CloudFormation } from 'aws-sdk'
+import {
+  CodePipelineClient,
+  CreatePipelineCommand,
+  ListPipelinesCommand,
+  CreatePipelineCommandInput,
+} from '@aws-sdk/client-codepipeline'
+import { PipelineSummary } from 'aws-sdk/clients/codepipeline'
+import * as dotenv from 'dotenv'
 
-export const handler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+dotenv.config()
+
+const codePipelineClient = new CodePipelineClient({ region: process.env.AWS_REGION })
+const cloudformation = new CloudFormation()
+
+export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   try {
-    let body = JSON.parse(event.body ?? '')
-    console.log(body)
+    let body = JSON.parse(event.body ?? '{}')
 
-    let branchName = body.ref.split('/').pop()
+    let branchName: string = body.ref.split('/').pop()
     console.log('Branch Name:', branchName)
+
+    // 既存のPipelineがあるかチェック
+    const existPipeline = await getExistPipeline(branchName)
+    if (existPipeline) {
+      console.log(`Pipeline ${existPipeline.name} already exists.`)
+
+      return {
+        statusCode: 200,
+        body: JSON.stringify('Finish! because Pipeline already exists'),
+      }
+    }
+
+    // Pipelineリソースを作成
+    await createPipeline(branchName)
 
     return {
       statusCode: 200,
-      body: JSON.stringify('Webhook received!'),
+      body: JSON.stringify(`Create AWS Resource for ${branchName}`),
     }
   } catch (e) {
     console.error(e)
@@ -19,5 +46,90 @@ export const handler = async (event: APIGatewayProxyEvent): Promise<APIGatewayPr
       statusCode: 500,
       body: JSON.stringify({ message: 'internal server error.' }),
     }
+  }
+}
+
+const getExistPipeline = async (branchName: string): Promise<PipelineSummary | undefined> => {
+  console.log(`start ${getExistPipeline.name}`)
+
+  try {
+    const listPipelinesOutput = await codePipelineClient.send(new ListPipelinesCommand({}))
+    const existPipeline = listPipelinesOutput.pipelines?.find((pipeline) => pipeline.name === `pipeline-${branchName}`)
+
+    return existPipeline
+  } catch (e) {
+    console.log(e)
+    throw e
+  } finally {
+    console.log(`end ${getExistPipeline.name}`)
+  }
+}
+
+const createPipeline = async (branchName: string): Promise<void> => {
+  console.log(`start ${createPipeline.name}`)
+
+  try {
+    // pipelineリソースを構築するための必要なロールやS3バケットキーを取得
+    const [roleArn, artifactBucketName] = await Promise.all([
+      getValueFromStackOutput(process.env.AWS_EXPORT_GITHUB_TRIGGER_PIPELINE_ROLE_ARN_KEY ?? ''),
+      getValueFromStackOutput(process.env.AWS_EXPORT_GITHUB_TRIGGER_PIPELINE_ARTIFACT_BUCKET_NAME_KEY ?? ''),
+    ])
+
+    const pipelineName = `pipeline-${branchName}`
+    const params = {
+      pipeline: {
+        name: pipelineName,
+        roleArn: roleArn,
+        artifactStore: {
+          location: artifactBucketName,
+          type: 'S3',
+        },
+        stages: [],
+        // その他の設定やステージの情報などを追加
+      },
+    }
+
+    await codePipelineClient.send(new CreatePipelineCommand(params))
+    console.log(`Created pipeline: ${pipelineName}`)
+  } catch (e) {
+    console.log(e)
+    throw e
+  } finally {
+    console.log(`end ${createPipeline.name}`)
+  }
+}
+
+const getValueFromStackOutput = async (key: string): Promise<string> => {
+  console.log(`start ${getValueFromStackOutput.name} key: ${key}`)
+
+  try {
+    const stackName = process.env.AWS_GITHUB_TRIGGER_STACK_NAME
+    const exportedOutputKey = key
+    const stack = await cloudformation.describeStacks({ StackName: stackName }).promise()
+
+    if (!stack || stack.Stacks?.length === 0 || !stack!.Stacks![0].Outputs) {
+      throw new Error('undefined stack outputs')
+    }
+
+    const outputs = stack.Stacks![0].Outputs
+    console.log(`outputs: ${outputs}`)
+    const output = outputs.find((o) => o.OutputKey === exportedOutputKey)
+    if (!output) {
+      throw new Error('undefined stack output')
+    }
+
+    const value = output.OutputValue
+    console.log(`output value : ${value}`)
+
+    if (!value) {
+      throw new Error('undefined value from output')
+    }
+
+    return value
+  } catch (e) {
+    console.log(e)
+    throw e
+  } finally {
+    console.log(`end ${getValueFromStackOutput.name}`)
   }
 }
--- infra/lib/github-trigger-stack.ts
+++ infra/lib/github-trigger-stack.ts
@@ -2,27 +2,58 @@ import * as cdk from 'aws-cdk-lib'
 import { Construct } from 'constructs'
 import * as lambda from 'aws-cdk-lib/aws-lambda'
 import * as apigateway from 'aws-cdk-lib/aws-apigateway'
+import * as iam from 'aws-cdk-lib/aws-iam'
+import * as s3 from 'aws-cdk-lib/aws-s3'
 import path = require('path')
 
 //  GithubへのPushに紐づいて実行されるLambdaを作成する
 export class GithubTriggerStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props)
+    super(scope, id, {
+      ...props,
+    })
 
+    // GithubへのPUSHでトリガーされるLambda
     const githubTriggerLambda = new cdk.aws_lambda_nodejs.NodejsFunction(this, 'githubTriggerLambda', {
       runtime: lambda.Runtime.NODEJS_18_X,
       handler: 'handler',
       entry: path.join(__dirname, '../handlers/github-trigger-lambda.ts'),
     })
 
+    // 上記のLambdaをAPIとして公開する
     const githubTriggerApi = new apigateway.LambdaRestApi(this, 'githubTriggerApi', {
       handler: githubTriggerLambda,
       restApiName: 'githubTriggerApi',
       deploy: true,
       proxy: false,
     })
-
     const webhook = githubTriggerApi.root.addResource('webhook')
-    webhook.addMethod('POST') // githubからのポストリクエストを受け入れる
+    // githubからのポストリクエストを受け入れる
+    webhook.addMethod('POST')
+
+    // pipeline用のRoleを作成してエクスポートしておく
+    const role = new iam.Role(this, 'githubTriggerPipelineRole', {
+      roleName: 'githubTriggerPipelineRole',
+      description: 'role for pipeline triggered by github event.',
+      assumedBy: new iam.ServicePrincipal('codepipeline.amazonaws.com'),
+    })
+    role.addManagedPolicy(iam.ManagedPolicy.fromAwsManagedPolicyName('AmazonS3FullAccess'))
+
+    new cdk.CfnOutput(this, 'exportGithubTriggerPipelineRoleArn', {
+      value: role.roleArn,
+      description: 'role for pipeline triggered by github event.',
+      exportName: 'exportGithubTriggerPipelineRoleArn', // .envの値を参照したい
+    })
+
+    // Pipelineのステージ間で共有するArtifactsを保管するS3バケットを生成してBucketネームをエクスポート
+    const artifactBucket = new s3.Bucket(this, 'githubTriggerPipelineArtifactBucket', {
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      autoDeleteObjects: true,
+    })
+    new cdk.CfnOutput(this, 'exportGithubTriggerPipelineArtifactBucketName', {
+      value: artifactBucket.bucketName,
+      description: 'bucket to store artifact sharing pipeline',
+      exportName: 'exportGithubTriggerPipelineArtifactBucketName', // .envの値を参照したい
+    })
   }
 }
