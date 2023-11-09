5aa4ec8409c926f80a7e50f3f7a625faa0d3e9f8
muratariku0903 <18ba179r@rikkyo.ac.jp>
2023-11-09 23:52:45 +0900

    wip

--- infra/handlers/github-trigger-lambda.ts
+++ infra/handlers/github-trigger-lambda.ts
@@ -238,6 +238,9 @@ const createCodeBuildProject = async (branchName: string, overwriting = true): P
         ],
       },
       serviceRole: roleArn,
+      buildBatchConfig: {
+        serviceRole: roleArn,
+      },
     }
 
     await codebuild.createProject(params).promise()
